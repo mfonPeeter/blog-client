ae740feaa4da7f15be13312e48c0420d0c82fb1e
mfonPeeter <godimfon@gmail.com>
2024-02-21 10:14:57 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blog Client
+
+This repository contains the frontend implementation of the blog-server repository (backend) for a blog post website.
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold underline">It Works!!!</div>
+    </>
+  );
+}
+
+export default App;
